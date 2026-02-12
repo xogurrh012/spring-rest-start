8ea2d52d325ce91a821e43579c611d1a70ce13ad
xogurrh012 <xogurrh012@gmail.com>
2026-02-12 17:42:48 +0900

    1

--- src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
+++ src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
@@ -4,13 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-//@Configuration
+import com.metacoding.springv2._core.filter.JwtAuthorizationFilter;
+
+@Configuration
 public class SecurityConfig {
 
     // 시큐리티 필터 등록
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 인증/권한 주소 커스텀마이징
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll());
+
+        // 폼 로그인 비활성화 ( POST : x-www-form-urlencoded : username, password )
+        http.formLogin(f -> f.disable());
+
+        // 베이직 인증 활성화 시킴 (request 할때마다 username, pasword를 요구) / 폼 로그인 비활성화를 하면 자동으로 활성화
+        // 되기 때문에 꺼야함함
+        http.httpBasic(b -> b.disable());
+
+        // 인증 필터를 변경
+        http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
--- src/main/java/com/metacoding/springv2/_core/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/metacoding/springv2/_core/filter/JwtAuthorizationFilter.java
@@ -1,7 +1,30 @@
 package com.metacoding.springv2._core.filter;
 
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 // 인가 필터
-public class JwtAuthorizationFilter {
-    
- 
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~JwtAuthorizationFilte~~~~~~~~~~~~~~~~~~~~~~~");
+
+        // localhost:8080/good?username=ssar&password=1234
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username.equals("ssar") && password.equals("1234")) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.getWriter().println("get out");
+        }
+    }
 }
